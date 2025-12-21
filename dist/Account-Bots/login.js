ca9484920d12cf1c82d97d3324631e1ab870aa05
hub-mgv <fire1230vvvvg@gmail.com>
2025-12-21 15:33:51 -0800

    Minecraft-Bot-Server

--- dist/Account-Bots/login.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.botLoginFunction = botLoginFunction;
-function botLoginFunction(myBot, passCode) {
-    myBot.chat(`/login ${passCode}`);
-    console.log(`${myBot.username} has logged in using pass ${passCode}`);
-}
